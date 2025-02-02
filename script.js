567af0bd2de7b33cf7ec652b7f8a00f94116b9fb
Marcus Schmid <24388408@sun.ac.za>
2025-02-02 15:30:10 +0200

    added timing and opacity

--- script.js
+++ script.js
@@ -30,8 +30,10 @@ window.onload = e => {
 
     let ctx = canvas.getContext("2d");
 
-    let yWindow = Math.min(parent.clientHeight, parent.clientWidth);
-    let xWindow = yWindow;
+    // let yWindow = Math.min(parent.clientHeight, parent.clientWidth);
+    // let xWindow = yWindow;
+    let yWindow = parent.clientHeight;
+    let xWindow = parent.clientWidth;
 
     let fontHeight = 16;
     let fontWidth = fontHeight * 11 / 20;
@@ -39,25 +41,31 @@ window.onload = e => {
     canvas.width = xWindow;
     canvas.height = yWindow;
 
-    let obj_count = 5;
+
+    // ----- params -----
+    let obj_count = 500;
+    ctx.fillStyle = "#ffffff";
+    let damping = 1;
 
     /** @type {obj[]} */
     let objs = [];
 
     for (let index = 0; index < obj_count; index++) {
         let pos = new vec(Math.random() * xWindow, Math.random() * yWindow);
-        let vel = new vec(0, 0);
-        let acc = new vec(0, 0.15);
+        let vel = new vec(Math.random() * 1000 - 500, Math.random() * 1000 - 500);
+        let acc = new vec(0, 500);
         // let acc = new vec((Math.random() - 0.5) * 3, (Math.random() - 0.5) * 3);
 
         let size = 30 * (Math.random() + 0.1);
         objs.push(new obj(pos, vel, acc, size));
     }
 
-    ctx.fillStyle = "#ffffff";
 
+    let prev_update = 0;
     function run_sim(time) {
         requestAnimationFrame(run_sim);
+
+        let dt = (time - prev_update) / 1000; // seconds
 
         // ctx.fillStyle = "#ffffff";
         ctx.fillRect(0, 0, xWindow, yWindow);
@@ -65,27 +73,30 @@ window.onload = e => {
         for (let i = 0; i < objs.length; i++) {
             let p = objs[i];
 
-            // ----- update -----
-            p.pos.x += p.vel.x;
-            p.pos.y += p.vel.y;
+            vel2 = p.vel.x ** 2 + p.vel.y ** 2;
+            ctx.strokeStyle = `rgba(0, 0, 0, ${vel2 / 500000})`;
 
-            p.vel.x += p.acc.x;
-            p.vel.y += p.acc.y;
+            // ----- update -----
+            p.pos.x += p.vel.x * dt;
+            p.pos.y += p.vel.y * dt;
+
+            p.vel.x += p.acc.x * dt;
+            p.vel.y += p.acc.y * dt;
 
             if (p.pos.x > xWindow - p.size) {
                 p.pos.x = xWindow - p.size;
-                p.vel.x *= -0.8;
+                p.vel.x *= -damping;
             } else if (p.pos.x < p.size) {
                 p.pos.x = p.size;
-                p.vel.x *= -0.8;
+                p.vel.x *= -damping;
             }
 
             if (p.pos.y > yWindow - p.size) {
                 p.pos.y = yWindow - p.size;
-                p.vel.y *= -0.8;
+                p.vel.y *= -damping;
             } else if (p.pos.y < p.size) {
                 p.pos.y = p.size;
-                p.vel.y *= -0.8;
+                p.vel.y *= -damping;
             }
 
             // if (Math.abs(p.vel.x) < 0.2) p.vel.x = 0;
@@ -93,10 +104,12 @@ window.onload = e => {
 
             // ----- draw -----
             ctx.beginPath();
-            // ctx.ellipse(p.pos.x, p.pos.y, p.size, p.size, 0, 0, 2 * Math.PI);
-            ctx.rect(p.pos.x - p.size, p.pos.y - p.size, p.size * 2, p.size * 2);
+            ctx.ellipse(p.pos.x, p.pos.y, p.size, p.size, 0, 0, 2 * Math.PI);
+            // ctx.rect(p.pos.x - p.size, p.pos.y - p.size, p.size * 2, p.size * 2);
             ctx.stroke();
         }
+
+        prev_update = time;
 
     }
 
